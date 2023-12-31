90bd970418bebcaa987182862f8e9b4c48da0c80
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-12-31 09:59:30 +0200

    q

--- src/js/createArrayElement.js
+++ src/js/createArrayElement.js
@@ -1,10 +1,13 @@
 const createArrayElement = arr => {
-  return arr.reduce((acc, item) => {
+  return arr.reduce
+  ((acc, item) => {
     acc.push(
       ` <li class='list-item'><a  href="${item.largeImageURL}"><img width="360"
            height="200"   src="${item.webformatURL}" alt="${item.tags}"></a><div class="cont">
           <div class="mini-cont">
             <p><strong>Likes</strong></p>
+
+  
             <p>${item.likes}</p> 
           </div>
           <div class="mini-cont">
